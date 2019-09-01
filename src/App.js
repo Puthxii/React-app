9d3f8df582ea4d1c21eb54b6ca26c208ba703834
Puth1998 <Teenaputh12@hotmail.com>
2019-09-01 10:05:59 +0700

    container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Containers from './containers/containers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  
+  render() {
+    return (
+      <Containers>
+       <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">Button</button>
+        </div>
+      </div>
+
+       
+      </Containers>
+    );
+  }
 }
+
 
 export default App;
