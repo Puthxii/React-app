d17bc76c7f2ad4055d98dc93057a3ab5b01b2ebb
Puth1998 <Teenaputh12@hotmail.com>
2019-09-01 13:46:47 +0700

    create layout

--- src/App.js
+++ src/App.js
@@ -1,61 +1,27 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Containers from './containers/containers';
-import TextInput from './components/TextInput';
-import Button from './components/Button';
+import  MenuBar  from './Layout/MenuBar';
+import  Header  from './Layout/Header';
+import  Content  from './Layout/Content';
+import  Footer  from './Layout/Footer';
 
 
 class App extends Component {
 
-  state = {
-    score: 0,
-    grade: ''
-  }
-
-  inputChangeHandler = (event) => {
-    let value = event.target.value;
-    this.setState({
-      score: value
-    });
-  }
-
-  calculateGradeHandler = () => {
-    let score = this.state.score;
-    let grade = '';
-    if(score >= 80){
-      grade = 'A'
-    }else if(score >= 70){
-      grade = 'B'
-    }else if(score >= 60){
-      grade = 'C'
-    }else if(score >= 50){
-      grade = 'D'
-    }else{
-      grade = 'F'
-
-    }
-
-    this.setState({
-      grade: grade
-    });
-  }
-
-
   render() {
     return (
-      
-      <Containers>
-       <div className="input-group mb-3">
-          <TextInput type="number" value= {this.state.score}  placeholder="input your score" classes="form-control" change={this.inputChangeHandler}/>
-        <div className="input-group-append">
-          <Button classes="btn btn-outline-secondary" click={this.calculateGradeHandler}>Click</Button>
-        </div>
+      <div>
+        <MenuBar></MenuBar>
+        <Header></Header>
+
+        <Containers>
+          <Content></Content>
+          <Footer></Footer>
+         </Containers>
+
       </div>
-      <h1>Grade : {this.state.grade}</h1>
-      
-      </Containers>
-    );
+     
+      );
   }
 }
 
--- src/containers/containers.js
+++ src/containers/containers.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import './containers.css';
 
 
 export default class Containers extends Component {
 
     render() {
         return(
-            <div className="container Mt-50" >
+            <div className="container" >
                 {this.props.children}
             </div>
         )
