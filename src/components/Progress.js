1a04c1990e132b0681262c7b20f76da60f25c1da
Puth1998 <Teenaputh12@hotmail.com>
2019-09-01 16:26:53 +0700

    create ability

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -5,12 +5,13 @@ export default class Progress extends Component {
     render() {
         return(
             
-            <div>
-                <label>C</label>
-                <div className="progress">
-                    <div className="progress-bar progress-bar-striped" role="progressbar" style={{width: '10%'}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">10%</div>
-                </div>
+        <div>
+            <label>{this.props.data.name}</label>
+            <div className="progress">
+                <div className={"progress-bar progress-bar-striped "+ this.props.data.color} role="progressbar" style={{width: this.props.data.percent}} aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">{this.props.data.percent}</div>
             </div>
+        </div>
+
  
         )
     }
--- src/components/ability/Ability.js
+++ src/components/ability/Ability.js
@@ -6,6 +6,26 @@ export default class Ability extends Component {
     
    
    // 80 >= bg-info, 65 >= bg-success, 50 >= bg-warning,  <= 50 bg-danger
+   state = {
+    language: [
+        {name: "C", percent: "80%", color: "bg-info"},
+        {name: "JAVA", percent: "85%", color: "bg-info"},
+        {name: "PHP", percent: "90%", color: "bg-info"},
+        {name: "PYTHON", percent: "40%", color: "bg-danger"},
+        {name: "HTML5", percent: "90%", color: "bg-info"},
+        {name: "C#", percent: "70%", color: "bg-success"},
+        {name: "JavaScript", percent: "84%", color: "bg-info"},
+        {name: "SQL", percent: "82%", color: "bg-info"},
+        {name: "XML", percent: "30%", color: "bg-danger"},
+        {name: "CSS", percent: "75%", color: "bg-success"},
+        {name: "jQuery", percent: "77%", color: "bg-success"},
+        {name: "REACT", percent: "60%", color: "bg-warning"},
+        {name: "REACT-NATIVE", percent: "66%", color: "bg-success"},
+        {name: "IONIC", percent: "20%", color: "bg-danger"},
+        {name: "ANGULAR", percent: "0%", color: "bg-danger"},
+        {name: "Shell-Script", percent: "0%", color: "bg-danger"}
+    ]
+   }
 
         render(){
             return (
@@ -15,7 +35,7 @@ export default class Ability extends Component {
                             <h4 className="my-0 font-weight-normal bg-dark text-white">Ability</h4>
                         </div>
                         <div className="card-body">
-                            {/* {this.state.language.map(obj => <Progress></Progress>)} */}
+                            {this.state.language.map(obj => <Progress data={obj}></Progress>)}
                         </div>
                     </div>  
                 </div>
